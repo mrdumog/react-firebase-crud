74dcad4b3622ea5858a48b6d1d1aafb9320273ed
Ishwar Acharya <55739073+mrdumog@users.noreply.github.com>
2023-05-20 23:33:42 +0000

    basic ui created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React & Firebase CRUD App</p>
       </header>
+      <div class="main">
+        <div class="form-block">
+          <div class="data-row">
+            <label>Name:</label>
+            <input type="text" />
+          </div>
+          <div class="data-row">
+            <label>Age:</label> <input type="text" />
+          </div>
+          <div class="data-row">
+            <label>City:</label> <input type="text" />
+          </div>
+          <div class="form-data-action">
+            <a href="#">Save</a>
+          </div>
+        </div>
+        <div class="data-block">
+          <div class="data-row">
+            <label>Name:</label>My Name
+          </div>
+          <div class="data-row">
+            <label>Age:</label>23
+          </div>
+          <div class="data-row">
+            <label>City:</label>Denver
+          </div>
+          <div class="data-action">
+            <a href="#">Edit</a>
+            <a href="#">Delete</a>
+          </div>
+        </div>
+        <div class="data-block">
+          <div class="data-row">
+            <label>Name:</label>My Name
+          </div>
+          <div class="data-row">
+            <label>Age:</label>23
+          </div>
+          <div class="data-row">
+            <label>City:</label>Denver
+          </div>
+          <div class="data-action">
+            <a href="#">Edit</a>
+            <a href="#">Delete</a>
+          </div>
+        </div>
+        <div class="data-block">
+          <div class="data-row">
+            <label>Name:</label>My Name
+          </div>
+          <div class="data-row">
+            <label>Age:</label>23
+          </div>
+          <div class="data-row">
+            <label>City:</label>Denver
+          </div>
+          <div class="data-action">
+            <a href="#">Edit</a>
+            <a href="#">Delete</a>
+          </div>
+        </div>
+        <div class="data-block">
+          <div class="data-row">
+            <label>Name:</label>My Name
+          </div>
+          <div class="data-row">
+            <label>Age:</label>23
+          </div>
+          <div class="data-row">
+            <label>City:</label>Denver
+          </div>
+          <div class="data-action">
+            <a href="#">Edit</a>
+            <a href="#">Delete</a>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        &copy; 2023{" An Opensource Project by "}
+        <a href="https://github.com/mrdumog" target="_new">
+          Ishwar Acharya
+        </a>
+      </div>
     </div>
   );
 }
